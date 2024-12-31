456f9fb8b06d5f3839570d8f6714c60d8189ff3b
AlfonsoG-dev <alfgomachi@gmail.com>
2024-12-31 00:10:47 -0500

    feat: use the utils of file to get the operations

---
+++ src/Application/Operations/FileOperations.java
@@ -0,0 +1,31 @@
+package Application.Operations;
+
+import Application.Utils.FileUtils;
+
+import java.util.List;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileOperations {
+    private String rootPath;
+    private FileUtils fileUtils;
+
+    public FileOperations(String rootPath) {
+        this.rootPath = rootPath;
+        this.fileUtils = new FileUtils(rootPath);
+    }
+
+    public List<File> getFilesOfDirectory(String dirPath) throws IOException {
+        File f = new File(dirPath);
+        if (!f.isDirectory()) throw new IOException("[Error] only directory type is allowed.");
+        return fileUtils.getDirectorFiles(Files.newDirectoryStream(f.toPath()));
+    }
+
+    public List<String> getLinesOfFile(String filePath) throws IOException {
+        if(!new File(filePath).isFile()) throw new IOException("[Error] only file type is allowed");
+        return fileUtils.getFileLines(filePath);
+    }
+
+}
