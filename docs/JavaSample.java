04ddb68bcc179a93166f0094332ee81a2500e874
AlfonsoG-dev <alfgomachi@gmail.com>
2025-01-01 17:53:33 -0500

    test: sample java file to test the parsing

---
+++ docs/JavaSample.java
@@ -0,0 +1,18 @@
+package docs;
+
+public class JavaSample {
+    private int age;
+    public JavaSample(int age) {
+        this.age = age;
+    }
+    public final static void greet() {
+        System.out.println("Hellow Mother");
+    }
+    public boolean isRunning(String b, String a,
+     int c, int m, String[] as) {
+        return false;
+    }
+    int count() {
+        return 0;
+    }
+}
