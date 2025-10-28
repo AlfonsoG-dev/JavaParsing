3639ff8de436944d6b5948fa126a2ad53f6fba17
AlfonsoG-dev <alfgomachi@gmail.com>
2025-10-27 23:41:40 -0500

    feat: Implement a method parser.
    1. From the list of lines filter the ones that indicates a method declaration.
    2. From the filtered list use the ones that match a java method syntax.
    > There are some methods that extend themself to more than 1 line.
    3. Where there is a multiline method append the other lines in 1 line.
    4. Create a list of method from the lines that match the previous descrive conditions
    5. Filter the methods to secure that only method declaration are in the list.

--- src/Application/Models/Methods.java
+++ src/Application/Models/Methods.java
@@ -13,12 +13,50 @@ public class Methods {
         List<String> n = List.copyOf(lines);
         return n;
     }
+    private boolean isMethod(String l) {
+        boolean m = false;
+        if(!l.contains(".") && (l.contains("(") || l.contains(")")) && l.length() > 1) {
+            m = true;
+        }
+        return m;
+    }
+    private boolean isMultiline(String l) {
+        boolean m = false;
+        if(l.endsWith(",")) {
+            m = true;
+        }
+        return m;
+    }
 
     public List<String> getMethods() {
+        List<String> methodLines = new ArrayList<>();
         List<String> methods = new ArrayList<>();
-        for(String l: getLines()) {
-            System.out.println(l + "nn");
+        for(int i=0; i<getLines().size(); ++i) {
+            String line = getLines().get(i).trim();
+            if(isMultiline(line)) {
+                StringBuffer mine = new StringBuffer();
+                int n = i;
+                while(true) {
+                    String cl = getLines().get(n).trim();
+                    if(cl.endsWith("{") || cl.endsWith(")")) {
+                        mine.append(cl);
+                        break;
+                    } else {
+                        mine.append(cl);
+                    }
+                    ++n;
+                }
+                methodLines.add(mine.toString().replace("{", ""));
+            } else if(isMethod(line)) {
+                methodLines.add(line.replace("{", ""));
+            }
         }
-        return null;
+        methods.addAll(
+            methodLines
+            .stream()
+            .filter(l -> l.contains("("))
+            .toList()
+        );
+        return methods;
     }
 }
