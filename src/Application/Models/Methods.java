dba217bd4f504157ed12f211f55430e1f9584f53
AlfonsoG-dev <alfgomachi@gmail.com>
2025-10-28 09:25:39 -0500

    feat: Parse method name from file.
    1. Move the declarations to the parser
    2. Use the declarations to find the method name

--- src/Application/Models/Methods.java
+++ src/Application/Models/Methods.java
@@ -4,59 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Methods {
-    private List<String> lines;
-    public Methods(List<String> lines) {
-        this.lines = lines;
+    private List<String> declarations;
+    public Methods(List<String> declarations) {
+        this.declarations = declarations;
     }
 
-    protected List<String> getLines() {
-        List<String> n = List.copyOf(lines);
-        return n;
-    }
-    private boolean isMethod(String l) {
-        boolean m = false;
-        if(!l.contains(".") && (l.contains("(") || l.contains(")")) && l.length() > 1) {
-            m = true;
-        }
-        return m;
-    }
-    private boolean isMultiline(String l) {
-        boolean m = false;
-        if(l.endsWith(",")) {
-            m = true;
-        }
-        return m;
-    }
 
-    public List<String> getMethodsDeclaration() {
-        List<String> methodLines = new ArrayList<>();
+    public List<String> getMethods() {
         List<String> methods = new ArrayList<>();
-        for(int i=0; i<getLines().size(); ++i) {
-            String line = getLines().get(i).trim();
-            if(isMultiline(line)) {
-                StringBuffer mine = new StringBuffer();
-                int n = i;
-                while(true) {
-                    String cl = getLines().get(n).trim();
-                    if(cl.endsWith("{") || cl.endsWith(")")) {
-                        mine.append(cl);
-                        break;
-                    } else {
-                        mine.append(cl);
-                    }
-                    ++n;
-                }
-                methodLines.add(mine.toString().replace("{", ""));
-            } else if(isMethod(line)) {
-                methodLines.add(line.replace("{", ""));
+        for(String d: declarations) {
+            if(d.contains("->")) {
+                methods.add(d.split(" ")[0]);
+            } else {
+                String stripArguments = d.split("\\(")[0];
+                String[] endPart = stripArguments.split(" ");
+                String name = endPart[endPart.length-1];
+                methods.add(name);
             }
         }
-        methods.addAll(
-            methodLines
-            .stream()
-            .filter(l -> l.contains("("))
-            .toList()
-        );
         return methods;
     }
+
 }
--- src/Application/Parser/FileParser.java
+++ src/Application/Parser/FileParser.java
@@ -5,6 +5,8 @@ import Application.Models.Methods;
 
 import java.io.File;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Optional;
 
 
@@ -13,15 +15,66 @@ import java.util.Optional;
  */
 public class FileParser {
     private String filePath;
+    private FileUtils fileUtils;
 
     public FileParser(String filePath) {
         this.filePath = Optional.ofNullable(filePath).orElse("." + File.separator);
+        fileUtils = new FileUtils("");
     }
 
+    protected List<String> getLines() {
+        List<String> n = List.copyOf(fileUtils.fileLines(filePath));
+        return n;
+    }
+    private boolean isMethod(String l) {
+        boolean m = false;
+        if(!l.contains(".") && (l.contains("(") || l.contains(")")) && l.length() > 1) {
+            m = true;
+        }
+        return m;
+    }
+    private boolean isMultiline(String l) {
+        boolean m = false;
+        if(l.endsWith(",")) {
+            m = true;
+        }
+        return m;
+    }
+
+    public List<String> getDeclarations() {
+        List<String> methodLines = new ArrayList<>();
+        List<String> methods = new ArrayList<>();
+        for(int i=0; i<getLines().size(); ++i) {
+            String line = getLines().get(i).trim();
+            if(isMultiline(line)) {
+                StringBuffer mine = new StringBuffer();
+                int n = i;
+                while(true) {
+                    String cl = getLines().get(n).trim();
+                    if(cl.endsWith("{") || cl.endsWith(")")) {
+                        mine.append(cl);
+                        break;
+                    } else {
+                        mine.append(cl);
+                    }
+                    ++n;
+                }
+                methodLines.add(mine.toString().replace("{", ""));
+            } else if(isMethod(line)) {
+                methodLines.add(line.replace("{", ""));
+            }
+        }
+        methods.addAll(
+            methodLines
+            .stream()
+            .filter(l -> l.contains("("))
+            .toList()
+        );
+        return methods;
+    }
     public final void parse() {
-        FileUtils fu = new FileUtils("");
-        Methods m = new Methods(fu.fileLines(filePath));
-        m.getMethodsDeclaration()
+        Methods m = new Methods(getDeclarations());
+        m.getMethods()
         .stream()
         .forEach(System.out::println);
     }
