3127e0733d76f9d4fe51f1f086ec6c1273c89228
AlfonsoG-dev <alfgomachi@gmail.com>
2025-10-27 22:39:06 -0500

    feat: testing fileUtils methods

--- src/JavaParsing.java
+++ src/JavaParsing.java
@@ -1,27 +1,9 @@
-import Application.Models.*;
-import Application.Utils.PrintFormat;
-
+import Application.Utils.FileUtils;
 class JavaParsing {
     public static void main(String[] args) {
-        Methods m = new Methods();
-        String filePath = "./src/Application/Models/Methods.java";
-        // String filePath = "./docs/JavaSample.java";
-        m.initializeLists(filePath);
-
-        Arguments a = new Arguments(m);
-        ReturnType r = new ReturnType(m);
-
-        PrintFormat print = new PrintFormat(filePath, m);
-
-        int total = m.getMethodsFromFile().size();
-        for(int i=0; i<total; ++i) {
-            String methodName = m.getMethodsName().get(i);
-            print.printLineMethod(
-                m.getMethodsFromFile().get(i),
-                methodName,
-                r.getReturnTypeOfMethod(methodName),
-                a.getArgumentsOfMethod(methodName)
-            );
-        }
+        FileUtils fu = new FileUtils("./src");
+        fu.fileLines("./src/Application/Utils/FileUtils.java")
+        .stream()
+        .forEach(System.out::println);
     }
 }
