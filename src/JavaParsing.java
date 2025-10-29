443847e3da248c2769510eb095891f7c47dd1bdf
AlfonsoG-dev <alfgomachi@gmail.com>
2025-10-29 18:50:49 -0500

    refactor: include two types of files to parse

--- src/JavaParsing.java
+++ src/JavaParsing.java
@@ -1,7 +1,8 @@
 import Application.Parser.FileParser;
 class JavaParsing {
     public static void main(String[] args) {
-        String f = "./docs/JavaSample.java";
+        String f = "./src/Application/Parser/FileParser.java";
+        // String f = "./docs/JavaSample.java";
         FileParser p = new FileParser(f);
         p.parse();
     }
