071de4f669e4a3306518a41472f1250b330c6c41
AlfonsoG-dev <alfgomachi@gmail.com>
2025-10-29 18:17:23 -0500

    test: add getArguments test

--- src/Test/MethodTest.java
+++ src/Test/MethodTest.java
@@ -9,14 +9,23 @@ public class MethodTest {
     private Methods methods;
 
     private final String[] TEST_RETURN_TYPES = {
-        "JavaSample", "void", "boolean", "int", "Opetators", "Person", "String"
+        "JavaSample", "void", "boolean", "int", "Opetators", "Person", "int"
+    };
+    private final String[] TEST_ARGUMENTS = {
+        "()",
+        "(int)",
+        "(String, String, int, int, String[])",
+        "()",
+        "(a, b)",
+        "(int, String, boolean)",
+        "(int, int)"
     };
 
     public MethodTest() {
         String[] rawDeclarations = {
             "public JavaSample()",
             "public final static void greet(int age)",
-            "public boolean isRunning(String b, String a,int c, int m,String[] as)",
+            "public boolean isRunning(String b, String a,\nint c, int m,\nString[] as)",
             "int count()",
             "Opetators o = (a, b) -> a + b;",
             "record Person(int age, String name, boolean married)",
@@ -34,16 +43,30 @@ public class MethodTest {
                     "Expected: " + expected.toString() + "\nReceive: " + receive.toString()
                 );
             }
-            System.out.println("[Info] No error where produce during test execution");
+            System.out.println("[Info] No error where produce during test execution of getReturnTypeTest");
         } catch(Exception e) {
             System.err.println("[Error] While trying to execute getReturnTypeTest:\n" + e.getLocalizedMessage());
         }
     }
-
+    public void getArgumentsTest() {
+        try {
+            List<String> receive = methods.getArguments();
+            List<String> expected = Arrays.asList(TEST_ARGUMENTS);
+            if(!receive.containsAll(expected)) {
+                throw new Exception(
+                    "Expected: " + expected.toString() + "\nReceive: " + receive.toString()
+                );
+            }
+            System.out.println("[Info] No error where produce during test execution of getArgumentsTest");
+        } catch (Exception e) {
+            System.err.println("[Error] While trying to execute getArgumentsTest:\n" + e.getLocalizedMessage());
+        }
+    }
 
     public static void main(String[] args) {
         MethodTest t = new MethodTest();
         t.getReturnTypeTest();
+        t.getArgumentsTest();
     }
     
 }
