9a592709eb60712955b44d11354cfdbbed904494
AlfonsoG-dev <alfgomachi@gmail.com>
2026-02-26 21:52:44 -0500

    refactor: rewrite proyect to use reflection

--- src/Application/JavaParsing.java
+++ src/Application/JavaParsing.java
@@ -1,10 +1,5 @@
 package application;
-import application.parser.FileParser;
 class JavaParsing {
     public static void main(String[] args) {
-        String f = "./src/Application/Parser/FileParser.java";
-        // String f = "./docs/JavaSample.java";
-        FileParser p = new FileParser(f);
-        p.parse();
     }
 }
--- src/Test/TestLauncher.java
+++ src/Test/TestLauncher.java
@@ -1,18 +1,6 @@
 package test;
-import test.cases.*;
 
 class TestLauncher {
-    private final static MethodTest mt = new MethodTest();
-    private final static FileParserTest fpt = new FileParserTest();
-
-    // TODO: invoke methods that end in *test* prefix using *reflection* on generic types.
-
     public static void main(String[] args) {
-        System.out.println("\n[Info] Executing MethodTest [TEST CASES]...");
-        mt.getArgumentsTest();
-        mt.getReturnTypeTest();
-        mt.getMethodNameTest();
-        System.out.println("\n[Info] Executing FileParserTest [TEST CASES]...");
-        fpt.getDeclarationsTest();
     }
 }
